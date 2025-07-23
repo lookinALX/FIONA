aab06f0b8bc2bfe0b43d3f652ce7ff426250ffaa
lookinALX <lookin.alx@gmail.com>
2025-07-23 22:32:21 +0200

    fiona

--- FileSorter.CLI/obj/Debug/net8.0/win-x64/FileSorter.CLI.AssemblyInfo.cs
+++ FileSorter.CLI/obj/Debug/net8.0/win-x64/FileSorter.CLI.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("YourName")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("lookinALX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("File sorting tool with AI-powered face recognition")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("File Intelligence Organization and Navigation Assistant")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a932db38fb236473c9fcaae0049fa9365394eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ce65d960de61e198de9af8304c3d26f77d76bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileSorter.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FIONA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
