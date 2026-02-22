aa18b0e7a7461dce9a6f0f027af4391c7cc51c05
lookinALX <lookin.alx@gmail.com>
2026-02-22 22:44:59 +0100

    m3 started

--- ml/classifier.py
+++ ml/classifier.py
@@ -8,8 +8,14 @@ from transformers import CLIPProcessor, CLIPModel
 
 model_name = "openai/clip-vit-base-patch32"
 
-model = CLIPModel.from_pretrained(model_name)
-processor = CLIPProcessor.from_pretrained(model_name)
+try:
+    model = CLIPModel.from_pretrained(model_name)
+    processor = CLIPProcessor.from_pretrained(model_name)
+    print("Model is loaded successfully")
+except OSError as e:
+    print("Model loading error:", e)
+except Exception as e:
+    print("Unexpected error:", e)
 
 app = fastapi.FastAPI()
 
